591c84a5098716ecaa1955cc0e62a9eb437b6b62
meliveloz <mvelozcampos@gmail.com>
2018-04-04 04:57:04 -0300

    input

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,28 +10,28 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-// $('button').click(getCompanies)
+$('#update').click(getCompanies)
 
-// function getCompanies() {
-//     createData($('input:checked').data('company'), $('option:selected').data('plan'))
-// }
+function getCompanies() {
+    createData($('input:checked').data('company'), $('option:selected').data('plan'))
+}
 
-// function createData(company, plan) {
-//     db.collection(company).doc(plan).collection('enjaja').add({
-//       nombre: $('#plan').val(),
-//       precio: $('#precio').val(),
-//       gb: $('#gb').val() + 'gb libres',
-//       minutos: $('#min').val(), 
-//       redes: $('#redes').val(), 
+function createData(company, plan) {
+    db.collection(company).doc(plan).collection('enjaja').add({
+      nombre: $('#plan').val(),
+      precio: $('#precio').val(),
+      gb: $('#gb').val() + 'gb libres',
+      minutos: $('#min').val(), 
+      redes: $('#redes').val(), 
       
-//     })
-//     .then(function(docRef) {
-//       console.log("Document written with ID: ", docRef.id);
-//     })
-//     .catch(function(error) {
-//       console.error("Error adding document: ", error);
-//     });
-// }
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+}
 
 function getData(arr) {
   arr.forEach((company) => {
@@ -40,7 +40,7 @@ function getData(arr) {
       querySnapshot.forEach((doc) => {
           console.log(doc.data());
           let k = doc.data()
-          $('#fijo').append(`<td>${k.minutos}</td><td>${k.gb}</td><td>${k.redes}</td>`)
+        $('#fijo').append(`<td>${k.minutos}</td><td>${k.gb}</td><td>${k.redes}</td><td>${k.precio}</td>`)
       });
     })
     let companyL = db.collection(company).doc('libre').collection('enjaja');
@@ -48,7 +48,7 @@ function getData(arr) {
       querySnapshot.forEach((doc) => {
           console.log(doc.data());
           let k = doc.data()
-          $('#libre').append(`<td>${k.minutos}</td><td>${k.gb}</td><td>${k.redes}</td>`)
+        $('#libre').append(`<td>${k.minutos}</td><td>${k.gb}</td><td>${k.redes}</td><td>${k.precio}</td>`)
       });
     })
   })
